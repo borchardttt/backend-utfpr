d0191b9ad5b61e35cae27292f2bbcd6eda92e9dc
Gabriel Borchardt <gabriel.borchardt@millpar.com>
2023-09-01 14:00:35 -0300

    add 3 new tests to pipeline

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,28 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_verificar_se_numero_e_impar()
+    {
+        $numero = 5;
+        $eImpar = $numero % 2 === 1;
+
+        $this->assertTrue($eImpar);
+    }
+
+    public function test_resultado_da_adicao()
+    {
+        $resultado = 2 + 3;
+
+        $this->assertEquals(5, $resultado);
+    }
+
+    public function test_array_tem_tres_elementos()
+    {
+        $array = [1, 2, 3];
+
+        $this->assertCount(3, $array);
+    }
+
+
 }
